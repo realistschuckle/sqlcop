4c65606f2b2dc023b8ed5449707610215fd29fe0
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2011-12-17 14:19:08 -0600

    Ensure AbstractSqlJudge implements IJudgeSql.

--- sqlcop.tests/AbstractSqlJudgeTests.cs
+++ sqlcop.tests/AbstractSqlJudgeTests.cs
@@ -9,6 +9,14 @@ namespace sqlcop.tests
 	public class AbstractSqlJudgeTests
 	{
 		[Test]
+		public void Implements_IJudgeSql()
+		{
+			Type type = typeof(AbstractSqlJudge);
+			Type iface = typeof(IJudgeSql);
+			Assert.That(iface.IsAssignableFrom(type));
+		}
+
+		[Test]
 		public void Canoncial_Name_Returns_Class_Full_Name()
 		{
 			AbstractSqlJudge stub;
