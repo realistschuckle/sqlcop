4ee7309385e5ab3eba2ec3b8fc2136321efebe22
Curtis Schlak <fuzzygeek1@users.sourceforge.net>
2011-12-30 23:46:31 -0600

    Set up the skeleton of the OPENXML tests.

--- sqlcop.tests/tsql/FromOpenxmlClauseParserTests.cs
+++ sqlcop.tests/tsql/FromOpenxmlClauseParserTests.cs
@@ -1,12 +1,29 @@
 using System;
+using NUnit.Framework;
+using sqlcop.tsql;
 
 namespace sqlcop.tests
 {
+	[TestFixture]
+	[Category("TSQL Parser")]
 	public class FromOpenxmlClauseParserTests
 	{
-		public FromOpenxmlClauseParserTests()
+		[SetUp]
+		public void RunBeforeEachTest()
 		{
+			_scanner = new Scanner();
+			_parser = new Parser(_scanner);
 		}
+		
+		private void CheckSelectStatement()
+		{
+			_scanner.SetSource("select * from " + _input, 0);
+			Assert.That(_parser.Parse(), "Failed on: " + _input);
+		}
+		
+		private string _input;
+		private Scanner _scanner;
+		private Parser _parser;
 	}
 }
 
